d50326af2f5b6061e36a7e5080da36f32b6dbbde
kaiyuleung <kyl1g16.ecs@gmail.com>
2022-09-28 09:38:10 +0100

    Made a favicon

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+// const catRoutes = require('./controllers/cats');
+// app.use('/cats', catRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello there!');
+});
+
+app.post('/', (req, res) => {
+    res.status(405).send('Not allowd!');
+});
+
+module.exports = app;
