ff165b4c9e12628b90375dceb91eeb6d2a74493f
kaiyuleung <kyl1g16.ecs@gmail.com>
2022-09-28 10:46:20 +0100

    set up data.js

--- server/data.js
+++ server/data.js
@@ -1,7 +1,23 @@
+const data = [
+    //10 results
+    {url: ,
+    title: ,
+    des:
+    },
+    {url: ,
+    title: ,
+    des:
+    },
+    //10 sponser results
+]
+
+
+
+
 class Data {
     constructor(){}
     static get all(){}
-    static findAndMatch(){}
+    
 
 
 }
