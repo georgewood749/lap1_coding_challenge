b6ef8a26cd95508b580a28c8d91a45e3b1ceea2b
kaiyuleung <kyl1g16.ecs@gmail.com>
2022-09-28 11:03:30 +0100

    write up script.js to display all results

--- client/script.js
+++ client/script.js
@@ -1,7 +1,26 @@
 const form = document.getElementById(`searchBar`);
 
-form.addEventListener('submit',search);
+// form.addEventListener('submit',search);
+form.addEventListener('submit',getAll);
 
+function getAll(){
+    fetch(`http://localhost:3000/database`)
+    .then(r => r.json())
+    .then(appendResults)
+    .catch(console.warn)
+}
+
+function appendResults(results){
+    const listOfResults = document.getElementById('results');
+    for (let result of results){
+        result = document.createElement('li');
+        res.textContent = `${results.title} <br> ${results.des}`;
+        listOfResults.append(result)
+    }
+        
+
+    
+}
 
 
 function search(e){
--- server/data.js
+++ server/data.js
@@ -1,14 +1,12 @@
 const data = [
     //10 results
-    {url: ,
-    title: ,
-    des:
-    },
-    {url: ,
-    title: ,
-    des:
-    },
+    
+    
     //10 sponser results
+    {url: "https://www.cat.com/",
+    title: "Cat.com | Cat | Caterpillar",
+    des: "Welcome! Select a region and language to continue. Select a Region, Africa, Middle-East, Asia, Australia, New Zealand, Eurasia, Europe, Latin ..."
+    }
 ]
 
 
